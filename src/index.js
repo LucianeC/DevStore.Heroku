36d033d8b1d556a69ab0483d2b4368df560c6af0
Luciane <costaluciane087@gmail.com>
2021-09-16 00:38:21 -0300

    terceiro

--- src/index.js
+++ src/index.js
@@ -1,72 +1,81 @@
 import db from './db.js';
 import express from 'express'
 import cors from 'cors'
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/matricula', async(req, resp) => {
-
+app.get('/produto', async (req, resp) => {
     try {
-        let alunos = await db.tb_matricula.findAll({ order:[['id_matricula','desc']] });
-        resp.send(alunos);
+        let produtos = await db.tb_produto.findAll({ order: [['id_produto', 'desc']] });
+        resp.send(produtos);
     } catch (e) {
-        resp.send({ erro: e.toString()})
-    }
-
-})
-
-app.post('/matricula', async(req, resp) => {
-    try {
-        let {nome, chamada, curso, turma} = req.body;
-
-        let r = await db.tb_matricula.create({
-            nm_aluno:nome,
-            nr_chamada:chamada,
-            nm_curso: curso,
-            nm_turma: turma
-        })
-        resp.send(r);
-    } catch (e) {
-        resp.send({ erro: e.toString()})
+        resp.send({ erro: e.toString() })
     }
 })
 
-app.put('/matricula:id', async(req, resp) => {
+app.post('/produto', async (req, resp) => {
     try {
-        let {nome, chamada, curso,turma} = req.body;
+        let { produto, categoria, precode, precopor, avaliacao, descricao, estoque, imagem } = req.body;
+        let consulta = await db.tb_produto.findOne({ where: {nm_produto: produto} })
+        if(consulta != null){
+            resp.send({erro: 'Esse produto já existe!'})
+        } else
+                if(produto == "" || categoria == "" || precode == "" || precopor == "" || avaliacao == "" || descricao == "" || estoque == "" || imagem == ""){
+                    resp.send({ erro: "Todos os campos são obrigatórios!" })
+                } else {
+                    let r = await db.tb_produto.create({
+                        nm_produto: produto,
+                        ds_categoria: categoria,
+                        vl_preco_de: precode,
+                        vl_preco_por: precopor, 
+                        vl_avaliacao: avaliacao, 
+                        ds_produto: descricao, 
+                        qtd_estoque: estoque, 
+                        img_produto: imagem
+                    })
+                    resp.send(r);
+                }
+    } catch (e) {
+        resp.send({ erro: 'Somente números nos campos: "Avaliação", "Preço POR", "Preço DE" e "Estoque"'})
+    }
+})
+
+app.put('/produto/:id', async (req, resp) => {
+    try {
+        let { produto, categoria, precode, precopor, avaliacao, descricao, estoque, imagem } = req.body;
         let { id } = req.params;
-
-        let r = await db.tb_matricula.update(
+        let r = await db.tb_produto.update(
             {
-                nm_aluno:nome,
-                nr_chamada:chamada,
-                nm_curso: curso,
-                nm_turma: turma
+                nm_produto: produto,
+                ds_categoria: categoria,
+                vl_preco_de: precode,
+                vl_preco_por: precopor, 
+                vl_avaliacao: avaliacao, 
+                ds_produto: descricao, 
+                qtd_estoque: estoque, 
+                img_produto: imagem
             },
             {
-                where: {id_matricula: id}
+                where: { id_produto: id }
             }
         )
         resp.sendStatus(200);
     } catch (e) {
-        resp.send({ erro: e.toString()})
+        resp.send({ erro: e.toString() })
     }
 })
 
-app.delete('/matricula:id', async(req, resp) => {
+app.delete('/produto/:id', async (req, resp) => {
     try {
         let { id } = req.params;
-
-        let r = await db.tb_matricula.destroy({where: {id_matricula: id} })
-        resp.sendStatus(200); 
+        let r = await db.tb_produto.destroy({ where: { id_produto: id } })
+        resp.sendStatus(200);
     } catch (e) {
-        resp.send({ erro: e.toString()})
+        resp.send({ erro: e.toString() })
     }
 })
 
 app.listen(process.env.PORT,
-
-x => console.log(`Server up at port ${process.env.PORT}`))
+    x => console.log(`Server up at port ${process.env.PORT}`))
